be5b7fbd754ee0d40a3b8f4eda47bb5fd162887c
Arthur Hwang <arthurhwangs@gmail.com>
2020-09-05 15:06:50 +0900

    edit, and fix ex_03.py

--- session04/ex_03.py
+++ session04/ex_03.py
@@ -7,3 +7,5 @@ secday = date % (60 * 60 * 24)
 hrs = secday // (60 * 60)
 mins = secday // 60 - hrs * 60
 secs = secday - hrs * 60 * 60 - mins * 60
+print(f'Number of days since the epoch: {int(days)} days')
+print(f'Current time in time of day in hours, minutes, and seconds: {int(hrs)}:{int(mins)}:{int(secs)}')
